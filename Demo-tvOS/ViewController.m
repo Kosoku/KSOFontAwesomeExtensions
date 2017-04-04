0e359816eb63acea0c0ac6b05d5c327cb1822ca3
William Towe <willbur1984@gmail.com>
2017-04-04 14:43:39 -0400

    added tvOS framework and demo targets

---
+++ Demo-tvOS/ViewController.m
@@ -0,0 +1,94 @@
+//
+//  ViewController.m
+//  Demo-tvOS
+//
+//  Created by William Towe on 4/4/17.
+//  Copyright © 2017 Kosoku Interactive, LLC. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#import "ViewController.h"
+
+#import <KSOFontAwesomeExtensions/KSOFontAwesomeExtensions.h>
+
+static CGSize const kItemSize = {.width=128, .height=128};
+
+@interface CollectionViewCell : UICollectionViewCell
+@property (assign,nonatomic) KSOFontAwesomeIcon icon;
+@property (strong,nonatomic) UIImageView *imageView;
+@end
+
+@implementation CollectionViewCell
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (!(self = [super initWithFrame:frame]))
+        return nil;
+    
+    _imageView = [[UIImageView alloc] initWithFrame:CGRectZero];
+    [_imageView setContentMode:UIViewContentModeCenter];
+    [self.contentView addSubview:_imageView];
+    
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    [self.imageView setFrame:self.contentView.bounds];
+}
+
+- (void)setIcon:(KSOFontAwesomeIcon)icon {
+    _icon = icon;
+    
+    [self.imageView setImage:[UIImage KSO_fontAwesomeImageWithIcon:self.icon size:kItemSize]];
+}
+
+@end
+
+@interface ViewController () <UICollectionViewDataSource,UICollectionViewDelegate>
+@property (strong,nonatomic) UICollectionView *collectionView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    
+    [layout setSectionInset:UIEdgeInsetsMake(8, 8, 0, 8)];
+    [layout setMinimumLineSpacing:8.0];
+    [layout setMinimumInteritemSpacing:8.0];
+    [layout setItemSize:kItemSize];
+    
+    [self setCollectionView:[[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:layout]];
+    [self.collectionView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.collectionView setBackgroundColor:[UIColor whiteColor]];
+    [self.collectionView registerClass:[CollectionViewCell class] forCellWithReuseIdentifier:NSStringFromClass([CollectionViewCell class])];
+    [self.collectionView setDataSource:self];
+    [self.collectionView setDelegate:self];
+    [self.view addSubview:self.collectionView];
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[view]|" options:0 metrics:nil views:@{@"view": self.collectionView}]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[top][view][bottom]" options:0 metrics:nil views:@{@"view": self.collectionView, @"top": self.topLayoutGuide, @"bottom": self.bottomLayoutGuide}]];
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return KSO_FONT_AWESOME_ICON_TOTAL_ICONS;
+}
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    CollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass([CollectionViewCell class]) forIndexPath:indexPath];
+    
+    [cell setIcon:(KSOFontAwesomeIcon)indexPath.row];
+    
+    return cell;
+}
+
+@end
