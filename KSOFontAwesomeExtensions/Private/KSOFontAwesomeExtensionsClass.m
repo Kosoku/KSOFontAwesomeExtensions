daf7fafd4045d566b2102ce7c3906dc942832fe8
William Towe <willbur1984@gmail.com>
2018-11-01 05:25:53 -0400

    add private class to use bundleForClass:

--- KSOFontAwesomeExtensions/Private/KSOFontAwesomeExtensionsClass.m
+++ KSOFontAwesomeExtensions/Private/KSOFontAwesomeExtensionsClass.m
@@ -1,8 +1,8 @@
 //
-//  NSBundle+KSOFontAwesomeExtensionsPrivate.m
+//  KSOFontAwesomeExtensionsClass.m
 //  KSOFontAwesomeExtensions
 //
-//  Created by William Towe on 10/15/17.
+//  Created by William Towe on 11/1/18.
 //  Copyright © 2018 Kosoku Interactive, LLC. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -13,13 +13,8 @@
 //
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#import "NSBundle+KSOFontAwesomeExtensionsPrivate.h"
 #import "KSOFontAwesomeExtensionsClass.h"
 
-@implementation NSBundle (KSOFontAwesomeExtensionsPrivate)
-
-+ (NSBundle *)KSO_fontAwesomeExtensionsFrameworkBundle; {
-    return [NSBundle bundleWithIdentifier:@"com.kosoku.ksofontawesomeextensions"] ?: [self bundleWithURL:[[self bundleForClass:KSOFontAwesomeExtensionsClass.class].resourceURL URLByAppendingPathComponent:@"KSOFontAwesomeExtensions.bundle"]] ?: [self bundleWithURL:[NSBundle.mainBundle URLForResource:@"KSOFontAwesomeExtensions" withExtension:@"bundle"]];
-}
+@implementation KSOFontAwesomeExtensionsClass
 
 @end
